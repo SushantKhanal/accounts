75b9b8364e53a4e220bb100e728a0b6c3dadfc69
SushantKhanal <dreamerking114@gmail.com>
2023-09-16 13:07:06 -0500

    logger added in rest controller

--- src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -14,6 +14,8 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +28,8 @@ import java.util.List;
 
 @RestController
 public class AccountsController {
+
+	private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
@@ -64,6 +68,7 @@ public class AccountsController {
 //	@CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
 	@Retry(name="retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
 	public CustomerDetails myCustomerDetails(@RequestHeader("eazybank-correlation-id") String correlationid, @RequestBody Customer customer) {
+		logger.info("myCustomerDetails() method started");
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 		List<Loans> loans = loansFeignClient.getLoansDetails(correlationid, customer);
 		List<Cards> cards = cardsFeignClient.getCardDetails(correlationid, customer);
